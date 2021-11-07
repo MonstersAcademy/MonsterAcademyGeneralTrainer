0af0f6415c3cc2dcbc1232dc9599363f10b8959a
SaharCarmel <saharsela271@gmail.com>
2021-11-07 10:34:16 +0200

    Now saving in data folder

--- montr/train.py
+++ montr/train.py
@@ -1,12 +1,18 @@
-from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.loggers import WandbLogger, wandb
 import yaml
 import pytorch_lightning as pl
 import torch
+import os
 from pytorch_lightning.callbacks import LearningRateMonitor
 from models.imageModels import ImagenetTransferLearning
 from dataloaders.mnistSample import MnistDataloader
 torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark = True
+
+
+def check_if_folder_exists(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
 
 def main(cfg):
@@ -15,6 +21,8 @@ def main(cfg):
 
     model = ImagenetTransferLearning(
         hparams=hparams)
+
+    check_if_folder_exists(hparams['logsDir'])
     wandb_logger = WandbLogger(
         save_dir=hparams['logsDir'],
         project=hparams['projectName'],
@@ -31,6 +39,7 @@ def main(cfg):
         mode="max",
         save_top_k=3,
         verbose=True,
+        dirpath=os.path.join(wandb_logger.experiment.dir, "checkpoints"),
     )
     wandb_logger.watch(model)
     lr_monitor = LearningRateMonitor(logging_interval='step')
